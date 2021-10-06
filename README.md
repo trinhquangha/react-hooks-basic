94dd4c0f69042f1ede1b3884afaa93684bd1b82d
trinhquangha <trinhha732000@gmail.com>
2021-10-06 18:38:08 +0700

    useState lab with TodoList: render and remove todo

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 # Getting Started with Create React App
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+	const [todoList, setTodoList] = useState([
+		{ id: 1, title: 'I love Easy Frontend! 😍 ' },
+		{ id: 2, title: 'We love Easy Frontend! 🥰 ' },
+		{ id: 3, title: 'They love Easy Frontend! 🚀 ' },
+	]);
+
+	function handleTodoClick(todo) {
+		const index = todoList.findIndex((x) => x.id === todo.id);
+
+		const newTodoList = [...todoList];
+		newTodoList.splice(index, 1);
+		setTodoList(newTodoList);
+	}
+
 	return (
 		<div className="app">
-			<h1>Welcom to React Hooks</h1>
-			<ColorBox />
+			<h1>React Hooks - TodoList</h1>
+			<TodoList todos={todoList} onTodoClick={handleTodoClick} />
 		</div>
 	);
 }
