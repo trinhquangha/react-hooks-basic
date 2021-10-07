ba1aab6cb91fc79d54dd60a6503b633e936e2052
trinhquangha <trinhha732000@gmail.com>
2021-10-07 21:58:39 +0700

    useEffect: call API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import PostList from './components/PostList';
 
 function App() {
 	const [todoList, setTodoList] = useState([
@@ -9,6 +10,25 @@ function App() {
 		{ id: 2, title: 'We love Easy Frontend! 🥰 ' },
 		{ id: 3, title: 'They love Easy Frontend! 🚀 ' },
 	]);
+
+	const [postList, setPostList] = useState([]);
+	useEffect(() => {
+		async function fetchPostList() {
+			try {
+				const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+				const response = await fetch(requestUrl);
+				const responseJSON = await response.json();
+				console.log(responseJSON);
+
+				const { data } = responseJSON;
+				setPostList(data);
+			} catch (error) {
+				console.error('Fail to fetch post list:', error.message);
+			}
+		}
+
+		fetchPostList();
+	}, []);
 
 	function handleTodoClick(todo) {
 		const index = todoList.findIndex((x) => x.id === todo.id);
@@ -18,21 +38,24 @@ function App() {
 		setTodoList(newTodoList);
 	}
 
-    function handleTodoFormSubmit(formValues) {
-        const newTodo = {
-            id: todoList.length + 10,
-            ...formValues,
-        }
+	function handleTodoFormSubmit(formValues) {
+		const newTodo = {
+			id: todoList.length + 10,
+			...formValues,
+		};
 		const newTodoList = [...todoList];
-        newTodoList.push(newTodo);
-        setTodoList(newTodoList);
-    }
+		newTodoList.push(newTodo);
+		setTodoList(newTodoList);
+	}
 
 	return (
 		<div className="app">
-			<h1>React Hooks - TodoList</h1>
-			<TodoForm onSubmit={handleTodoFormSubmit} />
-			<TodoList todos={todoList} onTodoClick={handleTodoClick} />
+			{/* <h1>React Hooks - TodoList</h1> */}
+			{/* <TodoForm onSubmit={handleTodoFormSubmit} />
+			<TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+
+			<h1>React Hooks - PostList</h1>
+			<PostList posts={postList} />
 		</div>
 	);
 }
---
+++ src/components/PostList/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+PostList.propTypes = {
+	posts: PropTypes.array,
+};
+
+PostList.defaultProps = {
+	posts: [],
+};
+
+function PostList({ posts }) {
+	return (
+		<ul className="post-list">
+			{posts.map((post) => (
+				<li key={post.id}>{post.title}</li>
+			))}
+		</ul>
+	);
+}
+
+export default PostList;
