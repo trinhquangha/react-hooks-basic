c4228ca48f572166707c882e5f61007a8db9f826
trinhquangha <trinhha732000@gmail.com>
2021-10-08 10:46:57 +0700

    useEffect: add Pagination

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
+import queryString from 'query-string';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
 import PostList from './components/PostList';
+import Pagination from './components/Pagination';
 
 function App() {
 	const [todoList, setTodoList] = useState([
@@ -11,24 +13,45 @@ function App() {
 		{ id: 3, title: 'They love Easy Frontend! 🚀 ' },
 	]);
 
+	const [pagination, setPagination] = useState({
+		_page: 1,
+		_limit: 10,
+		_totalRows: 1,
+	});
+
+	const [filters, setFilters] = useState({
+		_page: 1,
+		_limit: 10,
+	});
+
 	const [postList, setPostList] = useState([]);
 	useEffect(() => {
 		async function fetchPostList() {
 			try {
-				const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+				const paramsString = queryString.stringify(filters);
+				const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
 				const response = await fetch(requestUrl);
 				const responseJSON = await response.json();
 				console.log(responseJSON);
 
-				const { data } = responseJSON;
+				const { data, pagination } = responseJSON;
 				setPostList(data);
+				setPagination(pagination);
 			} catch (error) {
 				console.error('Fail to fetch post list:', error.message);
 			}
 		}
 
 		fetchPostList();
-	}, []);
+	}, [filters]);
+
+	function handlePageChange(newPage) {
+		console.log(newPage);
+		setFilters({
+			...filters,
+			_page: newPage,
+		});
+	}
 
 	function handleTodoClick(todo) {
 		const index = todoList.findIndex((x) => x.id === todo.id);
@@ -56,6 +79,7 @@ function App() {
 
 			<h1>React Hooks - PostList</h1>
 			<PostList posts={postList} />
+			<Pagination pagination={pagination} onPageChange={handlePageChange} />
 		</div>
 	);
 }
